e3e11ed15bb53b559f9a45dad3f4724703a45dad
rainyear <sniky.hack@gmail.com>
2015-06-21 20:19:14 +0800

    featured react-select

---
+++ libs/react-select-onAdd-lib/README.md
@@ -0,0 +1,3 @@
+copy ./*.js to node_modules/react-select/lib/
+
+check update of react-select
---
+++ libs/react-select-onAdd-lib/Value.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var React = require('react');
+
+var Option = React.createClass({
+
+	displayName: 'Value',
+
+	propTypes: {
+		label: React.PropTypes.string.isRequired
+	},
+
+	blockEvent: function blockEvent(event) {
+		event.stopPropagation();
+	},
+
+	render: function render() {
+		var label = this.props.label;
+
+		if (this.props.optionLabelClick) {
+			label = React.createElement(
+				'a',
+				{ className: 'Select-item-label__a',
+					onMouseDown: this.blockEvent,
+					onTouchEnd: this.props.onOptionLabelClick,
+					onClick: this.props.onOptionLabelClick },
+				label
+			);
+		}
+
+		return React.createElement(
+			'div',
+			{ className: 'Select-item' },
+			React.createElement(
+				'span',
+				{ className: 'Select-item-icon',
+					onMouseDown: this.blockEvent,
+					onClick: this.props.onRemove,
+					onTouchEnd: this.props.onRemove },
+				'×'
+			),
+			React.createElement(
+				'span',
+				{ className: 'Select-item-label' },
+				label
+			)
+		);
+	}
+
+});
+
+module.exports = Option;
